2fdd4aae214de3204174aab05175d234e3886c2c
Gus Smith <guscomps@gmail.com>
2022-09-14 23:00:24 +0000

    Remove remote

--- scripts/systolic_calyx.sh
+++ scripts/systolic_calyx.sh
@@ -10,13 +10,13 @@ fi
 mkdir -p results/systolic/futil
 ls benchmarks/systolic-sources/*.systolic | \
     parallel --bar $parallelism \
-             "fud e -q {} --to resource-estimate -s synth-verilog.remote 1 > results/systolic/futil/{/.}.json"
+             "fud e -q {} --to resource-estimate > results/systolic/futil/{/.}.json"
 
 # make sure results directory exists and run resource estimation
 mkdir -p results/systolic/futil-no-static
 ls benchmarks/systolic-sources/*.systolic | \
     parallel --bar $parallelism \
-             "fud e -q {} --to resource-estimate -s synth-verilog.remote 1 -s futil.flags '-d static-timing' > results/systolic/futil-no-static/{/.}.json"
+             "fud e -q {} --to resource-estimate -s futil.flags '-d static-timing' > results/systolic/futil-no-static/{/.}.json"
 
 # make results directory for futil-latency and run verilator
 mkdir -p results/systolic/futil-latency
